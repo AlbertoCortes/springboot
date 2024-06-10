3c2487b84256fc5e7179fb0cad1873eea8559336
AlbertoCts <c.n.albertocortes@gmail.com>
2024-06-09 22:33:39 -0600

    remove spaces

--- service-product/src/main/java/academy/digitallab/store/service_product/ServiceProductApplication.java
+++ service-product/src/main/java/academy/digitallab/store/service_product/ServiceProductApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceProductApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceProductApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceProductApplication.class, args);
+  }
 
-
-  
-}
